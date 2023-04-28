0b53dcd4e634fad473ceed064868423fa43885a1
gulen13 <vitaliy.gulen@gmail.com>
2023-04-28 22:46:32 +0300

    task-1 - done !

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import Profile from './profile/profile';
+
+import user from '../data/user.json';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        // alignItems: 'center',
+        padding: 50,
+        fontSize: 20,
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import css from './profile-css.module.css';
+
+const Profile = props => {
+  console.log(props);
+  const { avatar, location, stats, tag, username } = props;
+
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} width="100" />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>{'@' + tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        {Object.keys(stats).map(item => (
+          <li key={item} className={css.profileLi}>
+            <span className={css.label}>{item}</span>
+            <span className={css.quantity}>{stats[item]}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  stats: PropTypes.objectOf(PropTypes.number).isRequired
+}
+
+export default Profile;
